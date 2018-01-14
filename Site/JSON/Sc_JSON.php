0129ad265772f8af72688904f91ce84faf8ba342
Chandan Shastri <crshastri@gmail.com>
2018-01-14 23:25:21 +0530

    A Small Correction in JSON File.

--- Site/JSON/Sc_JSON.php
+++ Site/JSON/Sc_JSON.php
@@ -1,7 +1,7 @@
 <html>
 <body>
   <h3><b>Enter a number and string to Concat the Items.</b></h3><br>
-    <p id="demo"></p>
+
   <b>Number : </b><input name="number" id="num" type="number" min="0" required><br><br>
   <b>String&nbsp&nbsp : &nbsp&nbsp</b><input name="text" id="text" type="text" min="0" required>
   <br><br>&nbsp&nbsp<button type="button" onclick="CSConv()">Submit</button>
